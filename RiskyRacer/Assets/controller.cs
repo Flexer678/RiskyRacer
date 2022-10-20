6dc7c99047b2d68b13c479cf29a49fe4934796a8
Flexer678 <81823862+Flexer678@users.noreply.github.com>
2022-10-20 16:51:26 -0600

    chcked

--- RiskyRacer/Assets/controller.cs
+++ RiskyRacer/Assets/controller.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LEFTLANE : MonoBehaviour
-{   
-    public GameObject left_line;
+public class controller : MonoBehaviour
+{
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 10; i++){
-            GameObject clone =  Instantiate(left_line);
-            clone.transform.position = new Vector3(i*3,0f,0f);
-        }
         
     }
 
